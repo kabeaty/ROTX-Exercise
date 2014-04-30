f01b05d88c01e0508f5a52626a770d0436c112ca
Kathryn Beaty <kabeaty@gmail.com>
2014-04-29 21:17:34 -0500

    More refactoring and some clearer variable names

--- rotx.rb
+++ rotx.rb
@@ -2,22 +2,22 @@ require 'rubygems'
 require 'pry-debugger'
 
 def rotx(x, string, encrypt=true)
-  new_string = string.split("")
-  sol = new_string
+  string_arr = string.chars
+  sol = string_arr
   uppercase = ('A'..'Z').to_a
   lowercase = ('a'..'z').to_a
-  new_string.each_with_index do |item, index1|
-    if /[[:alpha:]]/.match(item)
-      if item == item.downcase then alph_array = lowercase else alph_array = uppercase end
-      alph_array.each_with_index do |letter, index2|
-        if item == letter
+  string_arr.each_with_index do |letter1, index1|
+    if /[[:alpha:]]/.match(letter1)
+      if letter1 == letter1.downcase then alph_arr = lowercase else alph_arr = uppercase end
+      alph_arr.each_with_index do |letter2, index2|
+        if letter1 == letter2
           if encrypt then index2 += x else index2 -= x end
           index2 -= 26 while index2 > 26
-          new_letter = alph_array[index2]
-          sol[index1] = new_letter
+          sol[index1] = alph_arr[index2]
         end
       end
     end
   end
   sol.join
 end
+
