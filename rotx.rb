f70e9c36bbd31e8ae723d2286182adfeaf31a3ae
Kathryn Beaty <kabeaty@gmail.com>
2014-04-30 17:53:33 -0500

    Small deletions for conciseness

--- rotx.rb
+++ rotx.rb
@@ -3,22 +3,21 @@ require 'pry-debugger'
 
 def rotx(x, string, encrypt=true)
   string_arr = string.chars
-  sol = string_arr
   uppercase = ('A'..'Z').to_a
   lowercase = ('a'..'z').to_a
   string_arr.each_with_index do |letter1, index1|
     if /[[:alpha:]]/.match(letter1)
-      if letter1 == letter1.downcase then alph_arr = lowercase else alph_arr = uppercase end
+      letter1 == letter1.downcase ? alph_arr = lowercase : alph_arr = uppercase
       alph_arr.each_with_index do |letter2, index2|
         if letter1 == letter2
-          if encrypt then index2 += x else index2 -= x end
+          encrypt ? index2 += x : index2 -= x
           index2 -= 26 while index2 > 26
           index2 += 26 while index2 < 0
-          sol[index1] = alph_arr[index2]
+          string_arr[index1] = alph_arr[index2]
         end
       end
     end
   end
-  sol.join
+  string_arr.join
 end
 
